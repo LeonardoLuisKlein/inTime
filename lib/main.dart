ed01eee4e93f8d4eb46db58383341630dcf00caf
LeonardoLuisKlein <leonardoluis.klein@outlook.com>
2023-10-05 22:10:59 -0300

    Feat: introducing home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:in_time/pages/principal.dart';
+import 'package:in_time/pages/login.dart';
+import 'package:in_time/pages/cadastro.dart';
 
 void main(List<String> app) {
   runApp(const MaterialApp(
--- lib/pages/principal.dart
+++ lib/pages/principal.dart
@@ -54,8 +54,127 @@ class _PrincipalState extends State<Principal> {
           ),
         ),
       ),
-      body: const Center(
-        child: Text("Aaaaa"),
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(top: 50.0),
+            child: Text(
+              "O que você deseja?",
+              style: TextStyle(
+                  fontSize: 32,
+                  fontFamily: 'Montserrat',
+                  color: Color.fromARGB(255, 0, 0, 0)),
+            ),
+          ),
+          Padding(
+              padding: const EdgeInsets.only(top: 50.0),
+              child: Center(
+                child: GestureDetector(
+                    onTap: () {
+                      print("paulo");
+                    },
+                    child: Container(
+                      width: 350,
+                      height: 80,
+                      decoration: const BoxDecoration(
+                        color: Color.fromARGB(255, 100, 21, 161),
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.only(left: 10.0),
+                            child: Text("Maquiagem",
+                                style: TextStyle(
+                                    fontSize: 30,
+                                    fontFamily: 'Montserrat',
+                                    color: Color.fromARGB(255, 255, 255, 255))),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 10.0),
+                            child: Image.asset(
+                              "images/blush.png",
+                              height: 60,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )),
+              )),
+          Padding(
+              padding: const EdgeInsets.only(top: 30.0),
+              child: Center(
+                child: GestureDetector(
+                    onTap: () {
+                      print("paulo");
+                    },
+                    child: Container(
+                      width: 350,
+                      height: 80,
+                      decoration: const BoxDecoration(
+                        color: Color.fromARGB(255, 100, 21, 161),
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.only(left: 10.0),
+                            child: Text("Skin care",
+                                style: TextStyle(
+                                    fontSize: 30,
+                                    fontFamily: 'Montserrat',
+                                    color: Color.fromARGB(255, 255, 255, 255))),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 30.0),
+                            child: Image.asset(
+                              "images/skin.png",
+                              height: 60,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )),
+              )),
+          Padding(
+              padding: const EdgeInsets.only(top: 30.0),
+              child: Center(
+                child: GestureDetector(
+                    onTap: () {
+                      print("paulo");
+                    },
+                    child: Container(
+                      width: 350,
+                      height: 80,
+                      decoration: const BoxDecoration(
+                        color: Color.fromARGB(255, 100, 21, 161),
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.only(left: 10.0),
+                            child: Text("Perfumaria",
+                                style: TextStyle(
+                                    fontSize: 30,
+                                    fontFamily: 'Montserrat',
+                                    color: Color.fromARGB(255, 255, 255, 255))),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 10.0),
+                            child: Image.asset(
+                              "images/perfume.png",
+                              height: 60,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )),
+              )),
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
