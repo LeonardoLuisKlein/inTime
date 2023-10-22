fe7d07d029b2cc9bd8e6f7fec4c4c99176838fad
Giani R Pertuzatti <giani.pertuzatti.pb@compasso.com.br>
2023-10-22 01:21:37 -0300

    feat: get product list and fix bottom navigator

--- lib/pages/principal.dart
+++ lib/pages/principal.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:in_time/pages/home.dart';
 import 'package:in_time/pages/pedidos.dart';
 import 'package:in_time/pages/perfil.dart';
-import 'package:in_time/services/auth_services.dart';
-import 'package:provider/provider.dart';
 
 class Principal extends StatefulWidget {
   const Principal({super.key});
@@ -51,126 +50,7 @@ class _PrincipalState extends State<Principal> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          const Padding(
-            padding: EdgeInsets.only(top: 50.0),
-            child: Text(
-              "O que você deseja?",
-              style: TextStyle(
-                  fontSize: 32,
-                  fontFamily: 'Montserrat',
-                  color: Color.fromARGB(255, 0, 0, 0)),
-            ),
-          ),
-          Padding(
-              padding: const EdgeInsets.only(top: 50.0),
-              child: Center(
-                child: GestureDetector(
-                    onTap: () {
-                      print("paulo");
-                    },
-                    child: Container(
-                      width: 350,
-                      height: 80,
-                      decoration: const BoxDecoration(
-                        color: Color.fromARGB(255, 100, 21, 161),
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Padding(
-                            padding: EdgeInsets.only(left: 10.0),
-                            child: Text("Maquiagem",
-                                style: TextStyle(
-                                    fontSize: 30,
-                                    fontFamily: 'Montserrat',
-                                    color: Color.fromARGB(255, 255, 255, 255))),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(right: 10.0),
-                            child: Image.asset(
-                              "images/blush.png",
-                              height: 60,
-                            ),
-                          ),
-                        ],
-                      ),
-                    )),
-              )),
-          Padding(
-              padding: const EdgeInsets.only(top: 30.0),
-              child: Center(
-                child: GestureDetector(
-                    onTap: () {
-                      print("paulo");
-                    },
-                    child: Container(
-                      width: 350,
-                      height: 80,
-                      decoration: const BoxDecoration(
-                        color: Color.fromARGB(255, 100, 21, 161),
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Padding(
-                            padding: EdgeInsets.only(left: 10.0),
-                            child: Text("Skin care",
-                                style: TextStyle(
-                                    fontSize: 30,
-                                    fontFamily: 'Montserrat',
-                                    color: Color.fromARGB(255, 255, 255, 255))),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(right: 30.0),
-                            child: Image.asset(
-                              "images/skin.png",
-                              height: 60,
-                            ),
-                          ),
-                        ],
-                      ),
-                    )),
-              )),
-          Padding(
-              padding: const EdgeInsets.only(top: 30.0),
-              child: Center(
-                child: GestureDetector(
-                    onTap: () => context.read<AuthService>().logout(),
-                    child: Container(
-                      width: 350,
-                      height: 80,
-                      decoration: const BoxDecoration(
-                        color: Color.fromARGB(255, 100, 21, 161),
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Padding(
-                            padding: EdgeInsets.only(left: 10.0),
-                            child: Text("Perfumaria",
-                                style: TextStyle(
-                                    fontSize: 30,
-                                    fontFamily: 'Montserrat',
-                                    color: Color.fromARGB(255, 255, 255, 255))),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(right: 10.0),
-                            child: Image.asset(
-                              "images/perfume.png",
-                              height: 60,
-                            ),
-                          ),
-                        ],
-                      ),
-                    )),
-              )),
-        ],
-      ),
+      body: _telas[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         iconSize: 35.0,
@@ -205,30 +85,15 @@ class _PrincipalState extends State<Principal> {
     );
   }
 
+  final List<Widget> _telas = [
+    const Home(),
+    const Pedidos(),
+    const Perfil(),
+  ];
+
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
-
-    switch (index) {
-      case 0:
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const Principal()),
-        );
-        break;
-      case 1:
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const Pedidos()),
-        );
-        break;
-      case 2:
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const Perfil()),
-        );
-        break;
-    }
   }
 }
